df7cd4acf5c95db1cd554f25d40126588ddf3e48
sushant verma <98468495+Vermasushant144@users.noreply.github.com>
2024-06-22 11:35:15 +0530

    Add files via upload

---
+++ pen-export-YzQXQGY/pen-export-YzQXQGY/dist/script.js
@@ -0,0 +1,27 @@
+const second = 1000,
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
+
+let countDown = new Date("April 09, 2024 12:00:00").getTime(),
+  x = setInterval(function () {
+    let now = new Date().getTime(),
+      distance = countDown - now;
+
+    (document.getElementById("days").innerHTML = Math.floor(distance / day)),
+      (document.getElementById("hours").innerHTML = Math.floor(
+        (distance % day) / hour
+      )),
+      (document.getElementById("minutes").innerHTML = Math.floor(
+        (distance % hour) / minute
+      )),
+      (document.getElementById("seconds").innerHTML = Math.floor(
+        (distance % minute) / second
+      ));
+
+    //do something later when date is reached
+    //if (distance < 0) {
+    //  clearInterval(x);
+    //  'IT'S MY BIRTHDAY!;
+    //}
+  }, second);
