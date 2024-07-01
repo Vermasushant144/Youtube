35a1a01d88e2a0345cf7bd40ac501ff1e35dffc9
sushant verma <98468495+Vermasushant144@users.noreply.github.com>
2024-07-01 13:30:01 +0530

    Add files via upload

---
+++ card-gang/dist/script.js
@@ -0,0 +1,12 @@
+// got bored 
+
+// const cards = document.querySelectorAll(".card");
+
+// cards.forEach((card) => {
+//   const observer = new IntersectionObserver(
+//     ([e]) => e.target.classList.toggle("stuck", e.intersectionRatio < 1),
+//     { threshold: [1] }
+//   );
+
+//   observer.observe(card);
+// });
